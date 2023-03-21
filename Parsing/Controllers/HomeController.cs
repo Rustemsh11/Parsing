7f1eed501306cd12ed390661af5335ac1dc59ae8
Rustem Shayheraziev <mr.shaykheraziev@mail.ru>
2023-03-21 21:14:10 +0300

    Добавьте файлы проекта.

---
+++ Parsing/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Parsing.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parsing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Pars()
+        {
+            var link = StroyPriceParsing.GetParsingLink("Бетон", "Пескобетон");
+            string page = StroyPriceParsing.GetStroyPricePage(link);
+            var list = StroyPriceParsing.ParsTovar(page);
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Parsing/Models/ParsingData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Parsing.Models
+{
+    public class ParsingData
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        public string RatingCompany { get; set; }
+        public string Price { get; set; }
+        public string Location { get; set; }
+        public string Comment { get; set; }
+        public string Image { get; set; }
+        public string UrlToCompany { get; set; }
+        public string Phone { get; set; }
+    }
+}
